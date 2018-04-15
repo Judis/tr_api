931c2267f79c8e93b0c11a6e60a449a3f2f35e63
Andrew Yudin <pasarian@gmail.com>
2018-04-15 14:23:26 +0600

    Change user validation logic to save password as hash

--- lib/api/accounts/user.ex
+++ lib/api/accounts/user.ex
@@ -14,6 +14,7 @@ defmodule I18NAPI.Accounts.User do
     field(:last_visited_at, :naive_datetime)
     field(:name, :string)
     field(:password, :string, virtual: true)
+    field(:password_confirmation, :string, virtual: true)
     field(:password_hash, :string)
     field(:restore_accepted_at, :naive_datetime)
     field(:restore_requested_at, :naive_datetime)
@@ -29,21 +30,38 @@ defmodule I18NAPI.Accounts.User do
     |> cast(attrs, [
       :name,
       :email,
-      :password_hash,
-      :is_confirmed,
-      :source,
-      :confirmation_token,
-      :restore_token,
-      :failed_sign_in_attempts,
-      :failed_restore_attempts,
-      :confirmed_at,
-      :confirmation_sent_at,
-      :restore_requested_at,
-      :restore_accepted_at,
-      :last_visited_at,
-      :invited_at
+      :password,
+      :password_confirmation,
+      :source
     ])
-    |> validate_required([:name, :email, :password])
+    |> validate_required([:name, :email, :password, :password_confirmation])
+    |> validate_changeset
+  end
+
+  @doc false
+  defp validate_changeset(struct) do
+    struct
+    |> validate_length(:email, min: 5, max: 255)
+    |> validate_format(:email, ~r/@/)
     |> unique_constraint(:email)
+    |> validate_length(:password, min: 8)
+    |> validate_format(
+      :password,
+      ~r/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*/,
+      message: "Must include at least one lowercase letter, one uppercase letter, and one digit"
+    )
+    |> validate_confirmation(:password)
+    |> generate_password_hash
+  end
+
+  @doc false
+  defp generate_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(password))
+
+      _ ->
+        changeset
+    end
   end
 end
