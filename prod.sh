3e3ef328df102b192f33915f2042f6006d614a11
Andrew Yudin <pasarian@gmail.com>
2018-05-11 09:24:10 +0600

    Fix build file

--- prod.sh
+++ prod.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker build -t tr_client_production -f Dockerfile.prod .
+docker build -t tr_api_production -f Dockerfile.prod .
 docker tag tr_api_production judis/app.l11n.api
 docker push judis/app.l11n.api
