cba46cd1d05f158432c13460570ae9c36a687b6c
Ivan Ivchenko <waltereevan@gmail.com>
2018-11-11 19:54:55 +0600

    Fix some controller tests

--- test/api_web/controllers/user_controller_test.exs
+++ test/api_web/controllers/user_controller_test.exs
@@ -1,42 +1,25 @@
 defmodule I18NAPIWeb.UserControllerTest do
+  use ExUnit.Case, async: true
+  @moduletag :user_controller
+
   use I18NAPIWeb.ConnCase
 
   alias I18NAPI.Accounts
   alias I18NAPI.Accounts.User
 
   @create_attrs %{
-    confirmation_sent_at: ~N[2010-04-17 14:00:00.000000],
-    confirmation_token: "some confirmation_token",
-    confirmed_at: ~N[2010-04-17 14:00:00.000000],
-    email: "some email",
-    failed_restore_attempts: 42,
-    failed_sign_in_attempts: 42,
-    invited_at: ~N[2010-04-17 14:00:00.000000],
-    is_confirmed: true,
-    last_visited_at: ~N[2010-04-17 14:00:00.000000],
-    name: "some name",
-    password_hash: "some password_hash",
-    restore_accepted_at: ~N[2010-04-17 14:00:00.000000],
-    restore_requested_at: ~N[2010-04-17 14:00:00.000000],
-    restore_token: "some restore_token",
-    source: "some source"
+    name: "test name",
+    email: "test@email.test",
+    password: "Qw!23456",
+    password_confirmation: "Qw!23456",
+    source: "test source"
   }
   @update_attrs %{
-    confirmation_sent_at: ~N[2011-05-18 15:01:01.000000],
-    confirmation_token: "some updated confirmation_token",
-    confirmed_at: ~N[2011-05-18 15:01:01.000000],
-    email: "some updated email",
-    failed_restore_attempts: 43,
-    failed_sign_in_attempts: 43,
-    invited_at: ~N[2011-05-18 15:01:01.000000],
-    is_confirmed: false,
-    last_visited_at: ~N[2011-05-18 15:01:01.000000],
-    name: "some updated name",
-    password_hash: "some updated password_hash",
-    restore_accepted_at: ~N[2011-05-18 15:01:01.000000],
-    restore_requested_at: ~N[2011-05-18 15:01:01.000000],
-    restore_token: "some updated restore_token",
-    source: "some updated source"
+    name: "test updated name",
+    email: "test_updated@email.test",
+    password: "Qw!23456",
+    password_confirmation: "Qw!23456",
+    source: "test source"
   }
   @invalid_attrs %{
     confirmation_sent_at: nil,
@@ -56,19 +39,41 @@ defmodule I18NAPIWeb.UserControllerTest do
     source: nil
   }
 
+  @fixture_user_attrs %{
+    name: "fixture name",
+    email: "fixture@email.test",
+    password: "Qw!23456",
+    password_confirmation: "Qw!23456",
+    source: "fixture source"
+  }
+
   def fixture(:user) do
+    {:ok, user} = Accounts.create_user(@fixture_user_attrs)
+    user
+  end
+
+  def fixture(:alter_user) do
     {:ok, user} = Accounts.create_user(@create_attrs)
     user
   end
 
   setup %{conn: conn} do
-    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+    user = fixture(:user)
+    {:ok, jwt, _claims} = I18NAPI.Guardian.encode_and_sign(user)
+    conn =
+      conn
+      |> put_req_header("accept", "application/json")
+      |> put_req_header("authorization", "Bearer #{jwt}")
+
+    {:ok, conn: conn}
   end
 
   describe "index" do
     test "lists all users", %{conn: conn} do
       conn = get(conn, user_path(conn, :index))
-      assert json_response(conn, 200)["data"] == []
+      [result|a] = json_response(conn, 200)["data"]
+      assert result["name"] == @fixture_user_attrs.name
+      assert result["email"] == @fixture_user_attrs.email
     end
   end
 
@@ -77,26 +82,9 @@ defmodule I18NAPIWeb.UserControllerTest do
       conn = post(conn, user_path(conn, :create), user: @create_attrs)
       assert %{"id" => id} = json_response(conn, 201)["data"]
 
-      conn = get(conn, user_path(conn, :show, id))
-
-      assert json_response(conn, 200)["data"] == %{
-               "id" => id,
-               "confirmation_sent_at" => ~N[2010-04-17 14:00:00.000000],
-               "confirmation_token" => "some confirmation_token",
-               "confirmed_at" => ~N[2010-04-17 14:00:00.000000],
-               "email" => "some email",
-               "failed_restore_attempts" => 42,
-               "failed_sign_in_attempts" => 42,
-               "invited_at" => ~N[2010-04-17 14:00:00.000000],
-               "is_confirmed" => true,
-               "last_visited_at" => ~N[2010-04-17 14:00:00.000000],
-               "name" => "some name",
-               "password_hash" => "some password_hash",
-               "restore_accepted_at" => ~N[2010-04-17 14:00:00.000000],
-               "restore_requested_at" => ~N[2010-04-17 14:00:00.000000],
-               "restore_token" => "some restore_token",
-               "source" => "some source"
-             }
+      assert %User{} = user = Accounts.get_user!(id)
+      assert user.name == @create_attrs.name
+      assert user.email == @create_attrs.email
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
@@ -106,35 +94,19 @@ defmodule I18NAPIWeb.UserControllerTest do
   end
 
   describe "update user" do
-    setup [:create_user]
 
-    test "renders user when data is valid", %{conn: conn, user: %User{id: id} = user} do
+    test "renders user when data is valid", %{conn: conn} do
+      user = fixture(:alter_user)
       conn = put(conn, user_path(conn, :update, user), user: @update_attrs)
-      assert %{"id" => ^id} = json_response(conn, 200)["data"]
+      assert %{"id" => id} = json_response(conn, 200)["data"]
 
-      conn = get(conn, user_path(conn, :show, id))
-
-      assert json_response(conn, 200)["data"] == %{
-               "id" => id,
-               "confirmation_sent_at" => ~N[2011-05-18 15:01:01.000000],
-               "confirmation_token" => "some updated confirmation_token",
-               "confirmed_at" => ~N[2011-05-18 15:01:01.000000],
-               "email" => "some updated email",
-               "failed_restore_attempts" => 43,
-               "failed_sign_in_attempts" => 43,
-               "invited_at" => ~N[2011-05-18 15:01:01.000000],
-               "is_confirmed" => false,
-               "last_visited_at" => ~N[2011-05-18 15:01:01.000000],
-               "name" => "some updated name",
-               "password_hash" => "some updated password_hash",
-               "restore_accepted_at" => ~N[2011-05-18 15:01:01.000000],
-               "restore_requested_at" => ~N[2011-05-18 15:01:01.000000],
-               "restore_token" => "some updated restore_token",
-               "source" => "some updated source"
-             }
+      assert %User{} = user = Accounts.get_user!(id)
+      assert user.name == @update_attrs.name
+      assert user.email == @update_attrs.email
     end
 
-    test "renders errors when data is invalid", %{conn: conn, user: user} do
+    test "renders errors when data is invalid", %{conn: conn} do
+      user = fixture(:alter_user)
       conn = put(conn, user_path(conn, :update, user), user: @invalid_attrs)
       assert json_response(conn, 422)["errors"] != %{}
     end
@@ -143,7 +115,8 @@ defmodule I18NAPIWeb.UserControllerTest do
   describe "delete user" do
     setup [:create_user]
 
-    test "deletes chosen user", %{conn: conn, user: user} do
+    test "deletes chosen user", %{conn: conn} do
+      user = fixture(:alter_user)
       conn = delete(conn, user_path(conn, :delete, user))
       assert response(conn, 204)
 
