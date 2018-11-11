272cd75f0d1b89d4d374feefd7aaa5fe01322359
Ivan Ivchenko <waltereevan@gmail.com>
2018-11-11 18:11:47 +0600

    Fix error view. Add test for all error statuses

--- test/api_web/views/error_view_test.exs
+++ test/api_web/views/error_view_test.exs
@@ -1,11 +1,25 @@
 defmodule I18NAPIWeb.ErrorViewTest do
+  use ExUnit.Case, async: true
+  @moduletag :error_view
+
   use I18NAPIWeb.ConnCase, async: true
 
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
+  test "renders 400.json" do
+    assert render(I18NAPIWeb.ErrorView, "400.json", []) ==
+             %{errors: %{detail: "Bad Request"}}
+  end
+
+  test "renders 401.json" do
+    assert render(I18NAPIWeb.ErrorView, "401.json", []) ==
+             %{errors: %{detail: "Unauthorized"}}
+  end
+
   test "renders 404.json" do
-    assert render(I18NAPIWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+    assert render(I18NAPIWeb.ErrorView, "404.json", []) ==
+             %{errors: %{detail: "Not Found"}}
   end
 
   test "renders 500.json" do
