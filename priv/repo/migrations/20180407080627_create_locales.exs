50a0f9ffbe89b1531a28aca6d220583e66cb2cba
Ivan Ivchenko <waltereevan@gmail.com>
2018-11-29 01:14:39 +0600

    Added new counter fields at schema

--- priv/repo/migrations/20180407080627_create_locales.exs
+++ priv/repo/migrations/20180407080627_create_locales.exs
@@ -12,6 +12,12 @@ defmodule I18NAPI.Repo.Migrations.CreateLocales do
       add :removed_at, :naive_datetime
       add :project_id, references(:projects, on_delete: :nothing)
 
+      add :count_of_not_verified_keys, :integer, default: 0
+      add :count_of_verified_keys, :integer, default: 0
+      add :count_of_translated_keys, :integer, default: 0
+      add :count_of_untranslated_keys, :integer, default: 0
+      add :count_of_keys_need_check, :integer, default: 0
+
       timestamps()
     end
 
