0ffc25827b30c0b9a9b581b200b968a3bd8ee81c
Ivan Ivchenko <waltereevan@gmail.com>
2018-12-14 15:03:52 +0600

    Fix test authorization

--- test/api_web/controllers/locale_controller_test.exs
+++ test/api_web/controllers/locale_controller_test.exs
@@ -23,38 +23,30 @@ defmodule I18NAPIWeb.LocaleControllerTest do
 
   def user_fixture(attrs \\ %{}) do
     {result, user} = Accounts.find_and_confirm_user(@user_attrs.email, @user_attrs.password)
-
-    if :error == result do
-      {:ok, user} =
-        attrs
-        |> Enum.into(@user_attrs)
-        |> Accounts.create_user()
-    end
-
     user
+    user = with :error <- result,
+                do: with {:ok, user} <- attrs |> Enum.into(@user_attrs) |> Accounts.create_user(),
+                    do: user
   end
 
   @valid_project_attrs %{
     name: "some name",
     default_locale: "en"
   }
-
-  def project_fixture(attrs \\ %{}, %User{} = user) do
-    {:ok, project} =
-      attrs
-      |> Enum.into(@valid_project_attrs)
-      |> Projects.create_project(user)
-
+  
+  def project_fixture(conn) do
+    {:ok, project} = @valid_project_attrs |> Projects.create_project(conn.user)
     project
   end
 
   setup %{conn: conn} do
     user = user_fixture()
-      {:ok, jwt, _claims} = I18NAPI.Guardian.encode_and_sign(user)
+    {:ok, jwt, _claims} = I18NAPI.Guardian.encode_and_sign(user)
     conn =
       conn
       |> put_req_header("accept", "application/json")
       |> put_req_header("authorization", "Bearer #{jwt}")
+      |> Map.put(:user, user)
 
     {:ok, conn: conn}
   end
@@ -98,39 +90,28 @@ defmodule I18NAPIWeb.LocaleControllerTest do
     removed_at: nil
   }
 
-  def fixture(:locale) do
-    user = user_fixture()
-    project_id = project_fixture(@valid_project_attrs, user).id
-    query = from(
-      p in Locale,
-      select: p,
-      where: p.project_id == ^project_id and p.locale == ^@create_attrs.locale
-    )
-    result = Repo.one(query)
+  def locale_fixture(project) do
+    default_locale = Translations.get_default_locale!(project.id)
 
-    if nil == result do
-      result = Translations.create_locale(@create_attrs, project_id)
+    if nil == default_locale do
+      {:ok, new_locale} = Translations.create_locale(@create_attrs, project.id)
+      new_locale
+    else
+      default_locale
     end
-
-    {:ok, locale} = result
-    locale
-  end
-
-  setup %{conn: conn} do
-    {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
 
   describe "index" do
     test "lists all locales", %{conn: conn} do
-      locale = fixture( :locale)
-      conn = get(conn, project_locale_path(conn, :index, locale.project_id, locale))
+      project = project_fixture(conn)
+      conn = get(conn, project_locale_path(conn, :index, project.id))
       assert json_response(conn, 200)["data"] == []
     end
   end
 
   describe "create locale" do
     test "renders locale when data is valid", %{conn: conn} do
-      project_id = fixture(:locale).project_id
+      project_id = project_fixture(conn).id
       conn = post(conn, project_locale_path(conn, :create, project_id), locale: @valid_attrs)
       assert %{"id" => id} = json_response(conn, 201)["data"]
       query = from(
@@ -140,35 +121,24 @@ defmodule I18NAPIWeb.LocaleControllerTest do
       )
       result_locale = Repo.one(query)
       assert result_locale.locale == @valid_attrs.locale
-
-#      conn = get(conn, project_locale_path(conn, :show, locale.project_id, id))
-
-#      assert json_response(conn, 200)["data"] == %{
-#               "id" => id,
-#               "count_of_keys" => 42,
-#               "count_of_translated_keys" => 42,
-#               "count_of_words" => 42,
-#               "is_default" => true,
-#               "is_removed" => true,
-#               "locale" => "another locale",
-#               "removed_at" => ~N[2010-04-17 14:00:00.000000]
-#             }
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
-      locale = fixture( :locale)
+      project = project_fixture(conn)
+      locale = locale_fixture(project)
       conn = post(conn, project_locale_path(conn, :create, locale.project_id), locale: @invalid_attrs)
       assert json_response(conn, 422)["errors"] != %{}
     end
   end
 
   describe "update locale" do
-    setup [:create_locale]
-
-    test "renders locale when data is valid", %{conn: conn, locale: %Locale{id: id} = locale} do
-      project_id = locale.project_id
+    test "renders locale when data is valid", %{conn: conn} do
+      project = project_fixture(conn)
+      locale = locale_fixture(project)
+      project_id = project.id
       conn = put(conn, project_locale_path(conn, :update, project_id, locale), locale: @update_attrs)
-      assert %{"id" => ^id} = json_response(conn, 200)["data"]
+      assert 1 = 2
+      #assert %{"id" => ^id} = json_response(conn, 200)["data"]
 
       query = from(
         p in Locale,
@@ -177,19 +147,6 @@ defmodule I18NAPIWeb.LocaleControllerTest do
       )
       result_locale = Repo.one(query)
       assert result_locale.locale == @update_attrs.locale
-
-#      conn = get(conn, project_locale_path(conn, :show, locale.project_id, id))
-
-#      assert json_response(conn, 200)["data"] == %{
-#               "id" => id,
-#               "count_of_keys" => 43,
-#               "count_of_translated_keys" => 43,
-#               "count_of_words" => 43,
-#               "is_default" => false,
-#               "is_removed" => false,
-#               "locale" => "some updated locale",
-#               "removed_at" => ~N[2011-05-18 15:01:01.000000]
-#             }
     end
 
     test "renders errors when data is invalid", %{conn: conn, locale: locale} do
@@ -199,7 +156,10 @@ defmodule I18NAPIWeb.LocaleControllerTest do
   end
 
   describe "delete locale" do
-    setup [:create_locale]
+    test "deletes chosen locale", %{conn: conn} do
+      project = project_fixture(conn)
+      locale = locale_fixture(project)
+      no_content_response = delete(conn, project_locale_path(conn, :delete, locale.project_id, locale))
 
     test "deletes chosen locale", %{conn: conn, locale: locale} do
       result = delete(conn, project_locale_path(conn, :delete, locale.project_id, locale))
@@ -218,10 +178,5 @@ defmodule I18NAPIWeb.LocaleControllerTest do
         get(conn, project_locale_path(conn, :show, locale.project_id, locale))
       end)
     end
-  end
-
-  defp create_locale(_) do
-    locale = fixture(:locale)
-    {:ok, locale: locale}
   end
 end
