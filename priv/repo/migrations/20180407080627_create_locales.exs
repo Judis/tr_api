b4de54c962a42b278a2dd7f9d55a7e49693d1121
Ivan Ivchenko <waltereevan@gmail.com>
2018-12-20 11:44:23 +0600

    Delete :need_check translation status as equal :unverified status

--- priv/repo/migrations/20180407080627_create_locales.exs
+++ priv/repo/migrations/20180407080627_create_locales.exs
@@ -15,13 +15,6 @@ defmodule I18NAPI.Repo.Migrations.CreateLocales do
       add :count_of_translated_keys, :integer, default: 0
       add :count_of_untranslated_keys, :integer, default: 0
 
-      add :total_count_of_translation_keys, :integer, default: 0
-      add :count_of_not_verified_keys, :integer, default: 0
-      add :count_of_verified_keys, :integer, default: 0
-      add :count_of_translated_keys, :integer, default: 0
-      add :count_of_untranslated_keys, :integer, default: 0
-      add :count_of_keys_need_check, :integer, default: 0
-
       timestamps()
     end
 
