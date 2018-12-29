c107d29ac2470085f57e4254e09cf18f6da44048
Ivan Ivchenko <waltereevan@gmail.com>
2018-12-29 10:16:40 +0600

    Refactoring

--- lib/api/utilites.ex
+++ lib/api/utilites.ex
@@ -10,13 +10,17 @@ defmodule I18NAPI.Utilites do
       %{field_a: value, field_b: "value"}
   """
   def key_to_atom(map) do
-    Enum.reduce(map, %{}, fn
-      {key, value}, acc when is_atom(key) -> Map.put(acc, key, value)
-      # String.to_existing_atom saves us from overloading the VM by
-      # creating too many atoms. It'll always succeed because all the fields
-      # in the database already exist as atoms at runtime.
-      {key, value}, acc when is_binary(key) -> Map.put(acc, String.to_existing_atom(key), value)
-    end)
+    Enum.reduce(
+      map,
+      %{},
+      fn
+        {key, value}, acc when is_atom(key) -> Map.put(acc, key, value)
+        # String.to_existing_atom saves us from overloading the VM by
+        # creating too many atoms. It'll always succeed because all the fields
+        # in the database already exist as atoms at runtime.
+        {key, value}, acc when is_binary(key) -> Map.put(acc, String.to_existing_atom(key), value)
+      end
+    )
   end
 
   @doc """
@@ -28,13 +32,19 @@ defmodule I18NAPI.Utilites do
       %{"field_a" => value, "field_b" => "value"}
   """
   def key_to_string(map) do
-    Enum.reduce(map, %{}, fn
-      {key, value}, acc when is_binary(key) -> Map.put(acc, key, value)
-      {key, value}, acc when is_atom(key) -> Map.put(acc, Atom.to_string(key), value)
-    end)
+    Enum.reduce(
+      map,
+      %{},
+      fn
+        {key, value}, acc when is_binary(key) -> Map.put(acc, key, value)
+        {key, value}, acc when is_atom(key) -> Map.put(acc, Atom.to_string(key), value)
+      end
+    )
   end
 
   def random_string(length) do
-    :crypto.strong_rand_bytes(length) |> Base.url_encode64() |> binary_part(0, length)
+    :crypto.strong_rand_bytes(length)
+    |> Base.url_encode64()
+    |> binary_part(0, length)
   end
 end
