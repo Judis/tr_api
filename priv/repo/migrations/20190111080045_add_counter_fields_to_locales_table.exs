eb1f171794b888ef72a884882855561be271ecdc
Ivan Ivchenko <waltereevan@gmail.com>
2019-01-11 14:06:07 +0600

    Fix migrations

--- priv/repo/migrations/20190111080045_add_counter_fields_to_locales_table.exs
+++ priv/repo/migrations/20190111080045_add_counter_fields_to_locales_table.exs
@@ -1,20 +1,19 @@
-defmodule I18NAPI.Repo.Migrations.CreateProjects do
+defmodule I18NAPI.Repo.Migrations.AddCounterFieldsToLocalesTable do
   use Ecto.Migration
 
   def change do
-    create table(:projects) do
-      add :name, :string
-      add :is_removed, :boolean, default: false, null: false
-      add :removed_at, :naive_datetime
+    alter table(:locales) do
 
       add :total_count_of_translation_keys, :integer, default: 0
       add :count_of_not_verified_keys, :integer, default: 0
       add :count_of_verified_keys, :integer, default: 0
-      add :count_of_translated_keys, :integer, default: 0
       add :count_of_untranslated_keys, :integer, default: 0
       add :count_of_keys_need_check, :integer, default: 0
 
-      timestamps()
+      modify :count_of_translated_keys, :integer, default: 0
+
+      remove :count_of_keys
+      remove :count_of_words
     end
 
   end
