95442d809417f0b595f515d8bcaa53be739f9a2c
Ivan Ivchenko <waltereevan@gmail.com>
2019-01-11 13:07:32 +0600

    Fix migration changes

--- priv/repo/migrations/20180407080601_create_projects.exs
+++ priv/repo/migrations/20180407080601_create_projects.exs
@@ -7,13 +7,6 @@ defmodule I18NAPI.Repo.Migrations.CreateProjects do
       add :is_removed, :boolean, default: false, null: false
       add :removed_at, :naive_datetime
 
-      add :total_count_of_translation_keys, :integer, default: 0
-      add :count_of_not_verified_keys, :integer, default: 0
-      add :count_of_verified_keys, :integer, default: 0
-      add :count_of_translated_keys, :integer, default: 0
-      add :count_of_untranslated_keys, :integer, default: 0
-      add :count_of_keys_need_check, :integer, default: 0
-
       timestamps()
     end
 
---
+++ priv/repo/migrations/20190111070400_create_projects.exs
@@ -0,0 +1,21 @@
+defmodule I18NAPI.Repo.Migrations.CreateProjects do
+  use Ecto.Migration
+
+  def change do
+    create table(:projects) do
+      add :name, :string
+      add :is_removed, :boolean, default: false, null: false
+      add :removed_at, :naive_datetime
+
+      add :total_count_of_translation_keys, :integer, default: 0
+      add :count_of_not_verified_keys, :integer, default: 0
+      add :count_of_verified_keys, :integer, default: 0
+      add :count_of_translated_keys, :integer, default: 0
+      add :count_of_untranslated_keys, :integer, default: 0
+      add :count_of_keys_need_check, :integer, default: 0
+
+      timestamps()
+    end
+
+  end
+end
