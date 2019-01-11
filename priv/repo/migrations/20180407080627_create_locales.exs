809409e1fbdc1e4091d8ed6c7ddb82cb0d9bae89
Ivan Ivchenko <waltereevan@gmail.com>
2019-01-11 15:36:54 +0600

    Rollback migrations to previous iteration

--- priv/repo/migrations/20180407080627_create_locales.exs
+++ priv/repo/migrations/20180407080627_create_locales.exs
@@ -5,12 +5,16 @@ defmodule I18NAPI.Repo.Migrations.CreateLocales do
     create table(:locales) do
       add :locale, :string
       add :is_default, :boolean, default: false, null: false
-      add :count_of_keys, :integer
-      add :count_of_words, :integer
-      add :count_of_translated_keys, :integer
       add :is_removed, :boolean, default: false, null: false
       add :removed_at, :naive_datetime
-      add :project_id, references(:projects, on_delete: :nothing)
+      add :project_id, references(:projects, on_delete: :delete_all)
+
+      add :total_count_of_translation_keys, :integer, default: 0
+      add :count_of_not_verified_keys, :integer, default: 0
+      add :count_of_verified_keys, :integer, default: 0
+      add :count_of_translated_keys, :integer, default: 0
+      add :count_of_untranslated_keys, :integer, default: 0
+      add :count_of_keys_need_check, :integer, default: 0
 
       timestamps()
     end
