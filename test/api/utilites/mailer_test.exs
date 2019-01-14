2e5b0b24010667bdd6407db527c709a15d29b3ef
Ivan Ivchenko <waltereevan@gmail.com>
2019-01-14 14:22:16 +0600

    Fix mailer api test

--- test/api/utilites/mailer_test.exs
+++ test/api/utilites/mailer_test.exs
@@ -1,8 +1,9 @@
 defmodule I18NAPI.MailerTest do
   use ExUnit.Case, async: true
-  use I18NAPI.DataCase
-
   @moduletag :mailer_api
+
+  use I18NAPI.DataCase
+  use I18NAPI.Fixtures, [:setup, :user, :project]
 
   import Swoosh.TestAssertions
 
@@ -10,61 +11,27 @@ defmodule I18NAPI.MailerTest do
   alias I18NAPI.Projects
   alias I18NAPI.UserEmail
   alias I18NAPI.Utilities
-  @user_attrs %{
-    name: "test name",
-    email: "test@email.test",
-    password: "Qw!23456",
-    password_confirmation: "Qw!23456",
-    source: "test source"
-  }
-
-  @additional_user_attrs %{
-    name: "additional name",
-    email: "additional@email.test",
-    password: "Qw!23456",
-    password_confirmation: "Qw!23456",
-    source: "additional source"
-  }
-
-  def user_fixture(attrs \\ %{}) do
-    {:ok, user} =
-      %User{}
-      |> User.changeset(attrs |> Enum.into(@user_attrs))
-      |> Repo.insert()
-
-    user |> Map.put(:confirmation_token, Utilities.random_string(25))
-  end
-
-  @valid_project_attrs %{
-    name: "some name",
-    default_locale: "en"
-  }
-
-  def project_fixture(attrs \\ %{}, %User{} = user) do
-    {:ok, project} =
-      attrs
-      |> Enum.into(@valid_project_attrs)
-      |> Projects.create_project(user)
-
-    project
-  end
 
   test "deliver confirmation email" do
-    UserEmail.create_confirmation_email(user_fixture()) |> I18NAPI.Mailer.deliver()
+    user = fixture(:user)
+    UserEmail.create_confirmation_email(user) |> I18NAPI.Mailer.deliver()
     assert_email_sent()
   end
 
   test "deliver restoration email" do
-    UserEmail.create_restoration_email(user_fixture()) |> I18NAPI.Mailer.deliver()
+    user = fixture(:user)
+    UserEmail.create_restoration_email(user) |> I18NAPI.Mailer.deliver()
     assert_email_sent()
   end
 
   test "deliver invitation email" do
-    user = user_fixture()
+    user = fixture(:user)
+    user_alter = fixture(:user_alter)
+    project = fixture(:project, user: user)
     UserEmail.create_invitation_email(
-      user_fixture(@additional_user_attrs),
+      user_alter,
       user,
-      project_fixture(user),
+      project,
       :translator,
       "some message"
     )
