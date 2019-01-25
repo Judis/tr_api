38709f9b75acb43b16ccc4fab6ef8dfa9464885d
Ivan Ivchenko <waltereevan@gmail.com>
2019-01-25 17:19:34 +0600

    Refactor parser caller

--- lib/api/parsers/parser.ex
+++ lib/api/parsers/parser.ex
@@ -1,27 +1,50 @@
 defmodule I18NAPI.Parsers do
-  @callback parse(String.t()) :: {:ok, term} | {:error, String.t()}
+  @callback parse(String.t()) :: {:ok, term} | {:error, :invalid_data} | {:error, :nil_found}
   @callback extensions() :: [String.t()]
   alias I18NAPI.Utilities
 
+  @doc """
+    Parse content of file with concrete extension
+
+      ## Examples
+
+          iex> parse(~s({"first": {"second": {"key": "value_1.2"}}}), "json")
+          response
+
+          iex> parse(~s(key, value), "abrakadabra")
+          {:error, :parser_not_defined}
+  """
+  def parse(content, _) when is_nil(content), do: {:error, :nil_found}
+
   def parse(content, ext) do
-    call_parser(get_module(ext), content)
+    ext
+    |> get_module()
+    |> call_parser(content)
   end
 
   defp call_parser([], content), do: {:error, :parser_not_defined}
 
   defp call_parser([module], content) do
-    Kernel.apply(module, :parse, [content])
-    |> Utilities.flatten_with_parent_key()
+    module
+    |> Kernel.apply(:parse, [content])
   end
 
   defp get_module(ext) do
     with {:ok, raw_list} <- :application.get_key(:api, :modules) do
       raw_list
-      |> Enum.filter(
-        &(String.starts_with?(to_string(&1), "Elixir.I18NAPI.Parsers") and
-            not String.equivalent?(to_string(&1), "Elixir.I18NAPI.Parsers"))
-      )
+      |> Enum.filter(&valid_module_name?(&1))
       |> Enum.filter(&(Kernel.apply(&1, :extensions, []) |> Enum.member?(ext)))
+    end
+  end
+
+  @included_module_prefix "Elixir.I18NAPI.Parsers"
+  @excluded_modules ["Elixir.I18NAPI.Parsers"]
+
+  defp valid_module_name?(name) do
+    name = to_string(name)
+
+    with String.starts_with?(name, @included_module_prefix) do
+      List.keymember?(@excluded_modules, name, 0)
     end
   end
 end
